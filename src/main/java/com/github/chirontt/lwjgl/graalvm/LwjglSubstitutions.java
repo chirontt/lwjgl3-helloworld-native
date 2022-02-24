75bab416b1d643fb15a6cfde4aafea7b3e443074
Tue Ton <49886739+chirontt@users.noreply.github.com>
2022-02-23 23:10:28 -0500

    update to use LWJGL 3.3.1 version
    with the caveat that the generated Windows native image would fail
    to work - it crashes when trying to load the glfw.dll library.

--- src/main/java/com/github/chirontt/lwjgl/graalvm/LwjglSubstitutions.java
+++ src/main/java/com/github/chirontt/lwjgl/graalvm/LwjglSubstitutions.java
@@ -20,13 +20,18 @@ final class Target_org_lwjgl_system_ThreadLocalUtil {
     @RecomputeFieldValue(kind = Kind.FromAlias, isFinal = true)
     private static long FUNCTION_MISSING_ABORT;
 
+    @Alias
+    @RecomputeFieldValue(kind = Kind.FromAlias, isFinal = true)
+    private static int CAPABILITIES_OFFSET;
+
     @Substitute
-    public static void setFunctionMissingAddresses(int functionCount, int index) {
+    public static void setFunctionMissingAddresses(int functionCount) {
+        long ptr = JNI_NATIVE_INTERFACE + CAPABILITIES_OFFSET;
         if (functionCount == 0) {
-            long missingCaps = memGetAddress(JNI_NATIVE_INTERFACE + Integer.toUnsignedLong(index) * POINTER_SIZE);
+            long missingCaps = memGetAddress(ptr);
             if (missingCaps != NULL) {
                 getAllocator().free(missingCaps);
-                memPutAddress(JNI_NATIVE_INTERFACE + Integer.toUnsignedLong(index) * POINTER_SIZE, NULL);
+                memPutAddress(ptr, NULL);
             }
         } else {
             long missingCaps = getAllocator().malloc(Integer.toUnsignedLong(functionCount) * POINTER_SIZE);
@@ -36,9 +41,10 @@ final class Target_org_lwjgl_system_ThreadLocalUtil {
 
             //the whole purpose of substituting this method is just to remove the following line
             //(which causes the generated native image to crash!)
-            //memPutAddress(JNI_NATIVE_INTERFACE + Integer.toUnsignedLong(index) * POINTER_SIZE, missingCaps);
+            //memPutAddress(ptr, missingCaps);
         }
     }
+
 }
 
 /** Dummy class with the file's name. */
